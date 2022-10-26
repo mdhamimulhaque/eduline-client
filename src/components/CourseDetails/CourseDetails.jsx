ee7ac76e174d91acd5e773e78493516811ce3b3f
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-10-27 02:46:12 +0600

    pdf done

--- src/components/CourseDetails/CourseDetails.jsx
+++ src/components/CourseDetails/CourseDetails.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import { Link, useLoaderData } from 'react-router-dom';
 import CourseOverview from './CourseOverview/CourseOverview';
+import Pdf from "react-to-pdf";
+
+
 
 const CourseDetails = () => {
     const courseDetailsData = useLoaderData()
     const { name, details, image_url, _id, title, category, overview } = courseDetailsData;
+
+    const pdfRef = useRef()
+
     return (
         <section>
             <div className="course-details_header_area bg-emerald-100">
@@ -13,7 +19,9 @@ const CourseDetails = () => {
                     <div className="container mx-auto">
                         <div className="flex flex-col md:flex-row gap-4 items-center justify-between">
                             <h2 className="text-6xl font-semibold">{name}</h2>
-                            <button className="block p-3 text-center flex  items-center justify-center gap-2 rounded-sm bg-emerald-400 hover:bg-emerald-500 text-white dark:text-gray-900 dark:bg-violet-400"><FaCloudDownloadAlt className='text-lg' /> pdf download</button>
+                            <Pdf targetRef={pdfRef} filename="code-example.pdf">
+                                {({ toPdf }) => <button onClick={toPdf} className="p-3 text-center flex  items-center justify-center gap-2 rounded-sm bg-emerald-400 hover:bg-emerald-500 text-white dark:text-gray-900 dark:bg-violet-400"><FaCloudDownloadAlt className='text-lg' /> pdf download</button>}
+                            </Pdf>
                         </div>
                     </div>
                 </div>
@@ -21,27 +29,29 @@ const CourseDetails = () => {
             <div className=" ">
                 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                     <div className="flex flex-col items-center justify-between lg:flex-row">
+
                         <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0">
-                            <div className="max-w-xl mb-6">
-                                <div>
-                                    <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider  uppercase rounded-full text-emerald-500">
-                                        {category}
+                            <section className='p-10 pdf_wrapper' ref={pdfRef}>
+                                <div className="max-w-xl mb-6">
+                                    <div>
+                                        <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider  uppercase rounded-full text-emerald-500">
+                                            {category}
+                                        </p>
+                                    </div>
+                                    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                                        {title}
+                                    </h2>
+                                    <p className="text-base text-gray-700 md:text-lg">
+                                        {details}
                                     </p>
                                 </div>
-                                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                                    {title}
-                                </h2>
-                                <p className="text-base text-gray-700 md:text-lg">
-                                    {details}
-                                </p>
-                            </div>
 
-                            <div className="course_overview">
-                                <h1 className="text-2xl font-bold mb-8">Course overview</h1>
-                                {overview.map((overVPoint, index) => <CourseOverview key={index} overVPoint={overVPoint} />)}
-                            </div>
+                                <div className="course_overview">
+                                    <h1 className="text-2xl font-bold mb-8">Course overview</h1>
+                                    {overview.map((overVPoint, index) => <CourseOverview key={index} overVPoint={overVPoint} />)}
+                                </div>
 
-
+                            </section>
 
                             <div className="mt-6">
                                 <Link to={`/checkout/${_id}`}>
@@ -49,6 +59,7 @@ const CourseDetails = () => {
                                 </Link>
                             </div>
                         </div>
+
                         <div className="relative lg:w-1/2">
                             <img
                                 className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
